c2de3969be0c3e11f4d58fe1aecd3041f9828c7a
Joe <95573961+The-Blue-W1zard@users.noreply.github.com>
2024-01-24 22:35:21 +0000

    changing to inherit button

--- Nea Maze Solving Application/obj/Debug/net8.0-windows/Nea Maze Solving Application.AssemblyInfo.cs
+++ Nea Maze Solving Application/obj/Debug/net8.0-windows/Nea Maze Solving Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Nea Maze Solving Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1b41ed7f727099fcf586fe72efe5bee14af72b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Nea Maze Solving Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Nea Maze Solving Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
